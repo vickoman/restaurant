222140c82b79afad894fac697fb43339428831e9
Victor Manuel <vicmandlagasca@gmail.com>
2014-10-03 17:07:40 -0500

    Habilitando el buscador

--- apps/restaurants/static/js/backbone/views/vistas.js
+++ apps/restaurants/static/js/backbone/views/vistas.js
@@ -3,6 +3,10 @@ var app = app || {};
 app.mainView = Backbone.View.extend({
 	el: '#app',
 
+	events: {
+		'keyup #buscador': 'buscarRestaurant'
+	},
+
 	initialize: function(){
 		app.restaurantsCollection.on('add', this.agregarRestaurant);
 		app.restaurantsCollection.fetch();
@@ -11,7 +15,27 @@ app.mainView = Backbone.View.extend({
 	agregarRestaurant: function(modelo){
 		var vista = new app.restaurantView({ model: modelo });
 		$('.list-group').append(vista.render().$el);
+	},
+
+	buscarRestaurant: function(){
+		var cadBuscador = $('#buscador').val().toLowerCase();
+		var filtro = app.restaurantsCollection.filter(function(modelo){
+			var cadModelo = modelo.get('name').substring(0, cadBuscador.length).toLowerCase();
+			if((cadBuscador === cadModelo) && (cadModelo.length == cadBuscador.length) && (cadBuscador.length != 0 ) && (cadModelo.length != 0 ))
+			{
+				return modelo;
+			}else if(cadModelo.length == 0 && cadBuscador.length == 0 ){
+				return modelo;
+			}
+		});
+		this.agregarFiltro(filtro);
+	},
+
+	agregarFiltro: function(coleccionFiltro){
+		this.$('.list-group').html('');
+		coleccionFiltro.forEach(this.agregarRestaurant, this);
 	}
+
 });
 app.restaurantView = Backbone.View.extend({
 	template: _.template($('#tplRestaurant').html()),
