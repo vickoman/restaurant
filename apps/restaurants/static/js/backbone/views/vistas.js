a6c31a3b5230ab6deadbd3d39cfb10b5a80837d1
Victor Manuel <vicmandlagasca@gmail.com>
2014-10-03 17:45:31 -0500

    Scroolinfinity backbone

--- apps/restaurants/static/js/backbone/views/vistas.js
+++ apps/restaurants/static/js/backbone/views/vistas.js
@@ -8,8 +8,9 @@ app.mainView = Backbone.View.extend({
 	},
 
 	initialize: function(){
-		app.restaurantsCollection.on('add', this.agregarRestaurant);
+		app.restaurantsCollection.on('add', this.agregarRestaurantFiltro);
 		app.restaurantsCollection.fetch();
+		this.llegofinal();
 	},
 
 	agregarRestaurant: function(modelo){
@@ -17,7 +18,15 @@ app.mainView = Backbone.View.extend({
 		$('.list-group').append(vista.render().$el);
 	},
 
+	agregarRestaurantFiltro: function(modelo){
+		if((window.cantidad >= modelo.get('id')) && (window.cantidad - 5 < modelo.get('id'))){
+			var vista = new app.restaurantView({model: modelo});
+			$('.list-group').append(vista.render().$el);
+		}
+	},
+
 	buscarRestaurant: function(){
+		window.stade = true;
 		var cadBuscador = $('#buscador').val().toLowerCase();
 		var filtro = app.restaurantsCollection.filter(function(modelo){
 			var cadModelo = modelo.get('name').substring(0, cadBuscador.length).toLowerCase();
@@ -25,6 +34,7 @@ app.mainView = Backbone.View.extend({
 			{
 				return modelo;
 			}else if(cadModelo.length == 0 && cadBuscador.length == 0 ){
+				window.stade = false;
 				return modelo;
 			}
 		});
@@ -33,7 +43,27 @@ app.mainView = Backbone.View.extend({
 
 	agregarFiltro: function(coleccionFiltro){
 		this.$('.list-group').html('');
+		if(window.stade === false){
+			coleccionFiltro = coleccionFiltro.filter(function(){
+				if(window.cantidad >= modelo.get('id'))
+				{
+					return modelo;
+				}
+			});
+		}
 		coleccionFiltro.forEach(this.agregarRestaurant, this);
+	},
+
+	llegofinal: function() {
+		var self = this;
+		$(window).scroll(function(){
+			if($(window).height() + $(window).scrollTop() == $(document).height()){				
+				if(window.stade === false){
+					window.cantidad = window.cantidad + 5;
+					app.restaurantsCollection.each(self.agregarRestaurantFiltro);
+				}
+			}
+		});
 	}
 
 });
